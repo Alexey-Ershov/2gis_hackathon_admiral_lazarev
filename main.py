a8bf816a1fe8a28044bf01bb9367e9d7fd3e2104
Alexey Ershov <aershov@rubackup.ru>
2025-10-04 20:39:09 +0300

    Basic CRUD

---
+++ main.py
@@ -0,0 +1,32 @@
+from fastapi import FastAPI, Depends, HTTPException
+from sqlalchemy.orm import Session
+import crud, models, schemas
+from database import engine, get_db
+
+# Создаем таблицы
+models.Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
+
+@app.post("/items/", response_model=schemas.Item)
+def create_item(item: schemas.ItemCreate, db: Session = Depends(get_db)):
+    return crud.create_item(db, item)
+
+@app.get("/items/{item_id}", response_model=schemas.Item)
+def read_item(item_id: int, db: Session = Depends(get_db)):
+    db_item = crud.get_item(db, item_id)
+    if db_item is None:
+        raise HTTPException(status_code=404, detail="Item not found")
+    return db_item
+
+@app.get("/items/", response_model=list[schemas.Item])
+def read_items(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    return crud.get_items(db, skip=skip, limit=limit)
+
+@app.delete("/items/{item_id}", response_model=schemas.Item)
+def delete_item(item_id: int, db: Session = Depends(get_db)):
+    item = crud.delete_item(db, item_id)
+    if item is None:
+        raise HTTPException(status_code=404, detail="Item not found")
+    return item
+
